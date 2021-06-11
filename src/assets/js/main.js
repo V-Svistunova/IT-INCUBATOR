f0fff0dbef04b7d222bd486941533d1ed5f6224a
Viktoryia-Svistunova <62076701+V-Svistunova@users.noreply.github.com>
2021-06-11 13:14:18 +0300

    refactor: change structure of the project

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,10 @@
 console.log(test(5, 2));
 console.log(test(2, 4));
 console.log(test(9, 1));
+
+document.getElementsByClassName('header__burger-btn').addEventListener('click', function() {
+  const c = document.getElementsByClassName('header__top');
+  c.toggle('header__top--active');
+  c.toggle('header__top');
+  document.getElementsByClassName('header__top').toggle('header__top--active');
+});
