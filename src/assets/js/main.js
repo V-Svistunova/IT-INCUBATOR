24cf8edca1186b0424770fc4c13b63f8a69bc5da
Viktoryia-Svistunova <62076701+V-Svistunova@users.noreply.github.com>
2021-08-21 14:05:04 +0300

    feat: add click from burgerMenu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,19 @@
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
+function burgerMenu() {
+  let burgerBtn = document.getElementById("burger");
+  
+  burgerBtn.addEventListener('click', toggleBurger);
+  burgerBtn.addEventListener('click', toggleMenu);
+}
 
-document.getElementsByClassName('header__burger-btn').addEventListener('click', function() {
-  const c = document.getElementsByClassName('header__top');
-  c.toggle('header__top--active');
-  c.toggle('header__top');
-  document.getElementsByClassName('header__top').toggle('header__top--active');
-});
+function toggleBurger() {
+  let burgerBtn = document.getElementById("burger");
+  burgerBtn.classList.toggle("header__burger--close");
+}
+
+function toggleMenu() {
+  let menu = document.querySelector(".header__top");
+
+  menu.classList.toggle("header__top--active");
+}
+
+burgerMenu();
