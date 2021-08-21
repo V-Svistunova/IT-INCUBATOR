9894c2432faeb635d65a31d945bb9fd0017b22b5
Viktoryia-Svistunova <62076701+V-Svistunova@users.noreply.github.com>
2021-08-22 00:12:27 +0300

    feat: add anchors

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -17,3 +17,16 @@ function toggleMenu() {
 }
 
 burgerMenu();
+
+let anchors = document.querySelectorAll('a[href*="#"]');
+
+for(let anchor of anchors) {
+  anchor.addEventListener('click', function(event) {
+    event.preventDefault();
+    let blockID = anchor.getAttribute('href');
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
