de47d887072a252abbdd81cdaf4691a1872a413c
Viktoryia-Svistunova <62076701+V-Svistunova@users.noreply.github.com>
2021-08-22 18:15:41 +0300

    feat: add scroll from fixBTn

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,27 +1,32 @@
-function burgerMenu() {
-  let burgerBtn = document.getElementById("burger");
-  
+const goTop = document.querySelector(".fix-btn");
+let burgerBtn = document.getElementById("burger");
+let menu = document.querySelector(".header__top");
+
+function openMenu() {  
   burgerBtn.addEventListener('click', toggleBurger);
   burgerBtn.addEventListener('click', toggleMenu);
 }
 
 function toggleBurger() {
-  let burgerBtn = document.getElementById("burger");
   burgerBtn.classList.toggle("header__burger--close");
 }
 
 function toggleMenu() {
-  let menu = document.querySelector(".header__top");
-
   menu.classList.toggle("header__top--active");
 }
 
-burgerMenu();
+function removeMenuAndClose() {
+  menu.classList.remove("header__top--active");
+  burgerBtn.classList.remove("header__burger--close");
+}
+
+openMenu();
 
 let anchors = document.querySelectorAll('a[href*="#"]');
 
 for(let anchor of anchors) {
   anchor.addEventListener('click', function(event) {
+    removeMenuAndClose();
     event.preventDefault();
     let blockID = anchor.getAttribute('href');
     document.querySelector(blockID).scrollIntoView({
@@ -30,3 +35,11 @@ for(let anchor of anchors) {
     })
   })
 }
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 300) {
+    goTop.style.display = 'flex'
+  } else {
+    goTop.style.display = 'none'
+  }
+});
