b83470be0d06f4211b0874fc5c2476e6394d40a2
V-Svistunova <wika1706@gmail.com>
2022-03-26 17:24:18 +0300

    feat: add adaptive burger-menu

--- Productivity/assets/js/main.js
+++ Productivity/assets/js/main.js
@@ -1,8 +1,12 @@
 $(function () {
 
-  $('.header__btn-menu').on('click', 
-    function () {
-    $('.header__btn-menu').toggleClass('header__btn-menu--active')
+  $('.header__btn-menu').on('click', function () {
+    $('.header__btn-menu').toggleClass('header__btn-menu--active'),
+    $('.header__nav').toggleClass('header__nav--active')
     });  
   
+    $('.nav-item').on('click', function () {
+      $('.header__btn-menu').removeClass('header__btn-menu--active'),
+      $('.header__nav').removeClass('header__nav--active')
+    });
 })
